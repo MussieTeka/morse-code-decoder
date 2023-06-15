1b1f2d5c30dc07d9718e13aa889671c25a6c49e1
emhamza <imumairhamza@gmail.com>
2023-06-15 05:28:28 -0700

    Create morese code decoder

--- morse_decoder.rb
+++ morse_decoder.rb
@@ -1,9 +1,29 @@
-MORSE_CODE_DICT = {
-  '.-'   => 'A', '-...' => 'B', '-.-.' => 'C', '-..'  => 'D', '.'    => 'E',
-  '..-.' => 'F', '--.'  => 'G', '....' => 'H', '..'   => 'I', '.---' => 'J',
-  '-.-'  => 'K', '.-..' => 'L', '--'   => 'M', '-.'   => 'N', '---'  => 'O',
-  '.--.' => 'P', '--.-' => 'Q', '.-.'  => 'R', '...'  => 'S', '-'    => 'T',
-  '..-'  => 'U', '...-' => 'V', '.--'  => 'W', '-..-' => 'X', '-.--' => 'Y',
+MORSE_CODE_DICT ={
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
   '--..' => 'Z'
 }.freeze
 
@@ -14,8 +34,8 @@ end
 puts decode_char('.--')
 
 def decode_word(morse_word)
-  morse_chars = morse_word.split(' ')
-  decoded_word = morse_chars.map { |char| decode_char(char) }.join('')
+  morse_chars = morse_word.split
+  decoded_word = morse_chars.map { |char| decode_char(char) }.join
 
   decoded_word
 end
@@ -23,8 +43,8 @@ end
 puts decode_char('A')
 
 def decode_message(morse_message)
-  morse_words = morse_message.split('   ')
-  decoded_message = morse_words.map { |word| decode_word(word) }.join(' ')
+  morse_words = morse_message.split
+  decoded_message = morse_words.map { |word| decode_word(word) }.join
 
   decoded_message
 end
